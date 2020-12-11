7f7ff56c9b796bc439ac56dfef76fb3b94616f8e
Brooke Miller <brookecm28@gmail.com>
2020-12-11 15:45:36 -0700

    Step 4 Complete

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const {CONNECTION_STRING, SESSION_SECRET} = process.env
+const authCtrl = require('./controllers/authController')
+const app = express()
+
+const PORT = 4000
+
+app.use(express.json())
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+        app.set('db', db)
+        console.log('DB Ready')
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+    })
+
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET
+    })
+)
+
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.get('/auth/logout', authCtrl.logout)
