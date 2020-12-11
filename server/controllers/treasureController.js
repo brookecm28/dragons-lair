59dc02f9b7183a32a210b0ac75d68718ad136382
Brooke Miller <brookecm28@gmail.com>
2020-12-11 16:54:51 -0700

    Step 7 Pt 1 Complete

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -9,5 +9,15 @@ module.exports = {
         const db = req.app.get('db')
         const userTreasure = await db.get_user_treasure([req.session.user.id])
         return res.status(200).send(userTreasure)
+    },
+
+    addUserTreasure: async (req, res) => {
+        const {treasureURL} = req.body
+        const {id} = req.session.user
+        const db = req.app.get('db')
+        console.log (treasureURL)
+        const userTreasure = await db.add_user_treasure([treasureURL, id])
+        console.log(userTreasure)
+        res.status(200).send(userTreasure)
     }
 }
